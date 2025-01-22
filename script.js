29f79bc88af79bab1bb2fcbb33dd3f729d2b7c91
aarush-348 <72818329+aarush-348@users.noreply.github.com>
2025-01-22 14:33:16 +0000

    Final project

--- script.js
+++ script.js
@@ -1,19 +1,52 @@
-container = document.querySelector("#container")
-
-let n = 100
-
-for(let i = 0; i < n; i++)
+function makeGrid(n)
 {
-    for(let j = 0; j < n; j++)
+    for(let i = 0; i < n; i++)
     {
-        const div = document.createElement("div")
-        div.classList.add("square")
-        container.appendChild(div)
+        for(let j = 0; j < n; j++)
+        {
+            const div = document.createElement("div")
+            div.classList.add("square")
+            container.appendChild(div)
 
-        div.style.height = `${540 / n}px`;
-        div.style.width = `${540 / n}px`;
+            div.style.height = `${size / n}px`;
+            div.style.width = `${size / n}px`;
+
+            const r = Math.random() * 256;
+            const g = Math.random() * 256;
+            const b = Math.random() * 256;
+
+            div.addEventListener('mouseenter', () => {
+                div.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+            })
+
+            clear.addEventListener('click', () =>{
+                div.style.backgroundColor = "white";
+            })
+        }
     }
 }
+
+let size = 960;
+
+container = document.querySelector("#container");
+
+container.style.height = `${size}px`
+container.style.width = `${size}px`
+
+makeGrid(16);
+
+resize = document.querySelector("#resize")
+clear = document.querySelector("#clear")
+
+resize.addEventListener("click", () => {
+    let new_num = prompt("Enter a size between 1 to 100");
+
+    if((new_num < 100) || (new_num > 1))
+    {
+        container.replaceChildren();
+        makeGrid(new_num);
+    }
+})
 
 
 
